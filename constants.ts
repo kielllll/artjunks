a249307cbe5e463df70a188307c6a9278c1ddec1
kielllll <eatarranza@gmail.com>
2022-09-25 00:42:21 +0800

    add: mock data

---
+++ constants.ts
@@ -0,0 +1,25 @@
+export const navLinks = [
+  'Home',
+  'About',
+  'Artists',
+  'Careers',
+  'News',
+  'Company',
+]
+
+export const news = [
+  {
+    imageUrl: 'morozov.png',
+    headline:
+      "France seeks to protect Russia's Morozov collection from seizure as masterpieces return home.",
+  },
+  {
+    imageUrl: 'muse_as_sea_creature.png',
+    headline:
+      "Picasso's muse as sea creature - will it break $100m at Sotheby's New York sale?",
+  },
+  {
+    imageUrl: 'venice_biennale.png',
+    headline: `Venice Biennale ${new Date().getFullYear()}: all the national pavillions, artists, and curators`,
+  },
+]
